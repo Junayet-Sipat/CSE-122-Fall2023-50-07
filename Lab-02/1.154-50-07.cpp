2014848d18e572339be40b59a408581b5f24e6ee
Junayet Hossain <122303096+Junayet-Sipat@users.noreply.github.com>
2023-01-17 09:34:33 +0600

    Add files via upload

---
+++ Lab-02/1.154-50-07.cpp
@@ -0,0 +1,17 @@
+// Codeforces problem no - 231A
+//Problem name - Team
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int a,b,c;
+    int s=0;
+    for (int i=0;i<n;i++){
+        cin >> a >> b >> c;
+        if (a+b+c>=2){
+            s += 1;}
+    }
+    cout << s;
+    }
